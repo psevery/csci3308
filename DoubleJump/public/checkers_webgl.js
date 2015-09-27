0f290729596785383a66cf346d551a3a7a0bbc0a
Peter Delevoryas <peter.delevoryas@colorado.edu>
2015-09-27 16:09:42 -0600

    Draws a square
    DESIGN DECISION:
    I decided to move all variables which exist for the entirety of the start()
    function to global scope. This is generally how game development is done,
    and in the past I have found it easier to reason about (contrary to what you
    might expect).
    Also, I've added the initBuffers function for initializing
    vertex buffer objects, and I added the draw function, for drawing
    to the canvas.

--- DoubleJump/public/checkers_webgl.js
+++ DoubleJump/public/checkers_webgl.js
@@ -1,14 +1,26 @@
 // FR-01 using WebGL context
 //
 
+// Generally, mutable global variables
+// are a bad thing. However, in the case
+// of game programming, I believe they
+// result in a more comprehensible program,
+// because the lifetime of all of these
+// variables is static.
+// MUTABLE GLOBAL VARS
+var glCanvas;               // HTML5 canvas DOM element, id="canvas"
+var gl;                     // WebGL context
+var squareVertexBuffer;     // Vertex buffer for drawing squares
+var vertexPositionAttribute // For sending vertices to shaders
+var shaderProgram;          // Shader program being used
+
 // Mozilla tutorials (and a lot of tutorials in general)
 //  embed the shaders as <script> elements in the html,
 //  then load them using a separate function, but since
 //  ECMAScript 6 allows for creating strings like this,
 //  I think it's easier and simple to just write the
 //  shader as part of the javascript file.
-
-// NOTE: All caps vars are considered global constants.
+//  NOTE: All caps vars are considered global constants.
 var VERTEX_SHADER_SOURCE = `
     attribute vec3 vpos;
 
@@ -16,56 +28,69 @@ var VERTEX_SHADER_SOURCE = `
         gl_Position = vec4(vpos, 1.0);
     }
 `;
-
 var FRAGMENT_SHADER_SOURCE = `
     void main(void) {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
     }
 `;
 
-// RETURN VALUE
-//  On success, returns an experimental-webgl context
-//  object. On failure, returns null and displays an
-//  alert message.
-function getWebGLContext(canvas) {
-    var gl = null;
+// USES GLOBAL VARS
+//  glCanvas
+//  gl
+function initWebGLContext() {
     try {
-        gl = canvas.getContext("experimental-webgl");
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
+        gl = glCanvas.getContext("experimental-webgl");
+        gl.viewportWidth = glCanvas.width;
+        gl.viewportHeight = glCanvas.height;
+
+        // Optional clear() call, set canvas to black
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
     } catch (e) {
         console.error(e)
     }
     if (!gl) {
-        //alert("Failed to initialize WebGL.");
         console.error("Failed to initialize WebGL.");
         gl = null;
+        return;
     }
-    return gl;
 }
 
-// RETURN VALUE
-//  On success, returns the context passed in
-//  with a compiled shader program and initialized
-//  attribute arrays. On failure, returns null and
-//  displays an alert message.
-function compileShaders(gl, vertexShaderSource, fragmentShaderSource) {
+// PARAMS
+//  vertexShaderSource: shader script as string
+//  fragmentShaderSource: shader script as string
+// USES GLOBAL VARS
+//  gl
+function initShaderProgram(vertexShaderSource, fragmentShaderSource) {
     // Compile shaders, handle errors
+    // The compilation of individual shaders could be split into a
+    // separate function, but personally I think it's easier to
+    // handle errors like this. I'll leave it up to you guys if you
+    // would like to change it.
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSource);
     gl.compileShader(vertexShader);
     if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        //alert("Error compiling vertex shader: " + gl.getShaderInfoLog(vertexShader));
-        console.error("Error compiling vertex shader: " + gl.getShaderInfoLog(vertexShader));
-        return null;
+        console.error(
+            "Error compiling vertex shader: " +
+            gl.getShaderInfoLog(vertexShader)
+        );
+        gl = null;
+        return;
     }
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
     gl.compileShader(fragmentShader);
     if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        //alert("Error compiling fragment shader: " + gl.getShaderInfoLog(fragmentShader));
-        console.error("Error compiling fragment shader: " + gl.getShaderInfoLog(fragmentShader));
-        return null;
+        console.error(
+            "Error compiling fragment shader: " +
+            gl.getShaderInfoLog(fragmentShader)
+        );
+        gl = null;
+        return;
     }
 
     // Link shaders into final program
@@ -74,34 +99,75 @@ function compileShaders(gl, vertexShaderSource, fragmentShaderSource) {
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        //alert("Unable to intialize the shader program.");
         console.error("Unable to intialize the shader program.");
-        return null;
+        gl = null;
+        return;
     }
     gl.useProgram(shaderProgram);
 
     // Initialize shader globals
     vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vpos");
     gl.enableVertexAttribArray(vertexPositionAttribute);
+}
 
-    return gl;
+// USES
+//  gl
+//  squareVertexBuffer
+function initBuffers() {
+    try {
+        squareVertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexBuffer);
+
+        var vertices = [
+             1.0,  1.0, 0.0,
+            -1.0,  1.0, 0.0,
+             1.0, -1.0, 0.0,
+            -1.0, -1.0, 0.0,
+        ];
+
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(vertices),
+            gl.STATIC_DRAW
+        );
+    } catch (e) {
+        console.error(e);
+        gl = null;
+        return;
+    }
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexBuffer);
+    gl.vertexAttribPointer(
+        vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0
+    );
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
 function start() {
-    var canvas = document.getElementById("canvas");
+    glCanvas = document.getElementById("canvas");
 
-    gl = getWebGLContext(canvas);
+    initWebGLContext(canvas);
     if (!gl) {
         console.error("Error getting WebGL Context, exiting...");
         return;
     }
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    gl = compileShaders(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+
+    initShaderProgram(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
     if (!gl) {
         console.error("Error compiling shaders, exiting...");
         return;
     }
+
+    initBuffers();
+    if (!gl) {
+        console.error("Error intializing vertex buffers, exiting...");
+        return;
+    }
+
+    // Set draw loop
+    setInterval(draw, 15);
 }
