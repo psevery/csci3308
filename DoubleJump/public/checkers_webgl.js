6135ec39c4af47348edbbce6bef0a72cece386c5
Peter Delevoryas <peter.delevoryas@colorado.edu>
2015-09-27 21:53:29 -0600

    Flattened the code a bit
    I removed a lot of the function calls, as I think that
    obscures the program's execution a lot. For now, I think
    it's better to put everything in one big function, and
    then refactor later, when it's clear that refactoring
    would be beneficial.

--- DoubleJump/public/checkers_webgl.js
+++ DoubleJump/public/checkers_webgl.js
@@ -1,173 +1,148 @@
-// FR-01 using WebGL context
-//
+// Anatomy of the WebGL rendering process
+// 1. Create a canvas element
+// 2. Obtain a rendering context for the canvas element
+// 3. Initialize the viewport which you will render into
+// 4. Create one or more buffers containing the data to be rendered (vertices)
+// 5. Create one or more matrices to transform the vertex data to pixel locations
+// 6. Create one or more shaders to implement the rendering algorithm
+// 7. Initialize the shaders with their data parameters
+// 8. Render the data to the screen
 
-// Generally, mutable global variables
-// are a bad thing. However, in the case
-// of game programming, I believe they
-// result in a more comprehensible program,
-// because the lifetime of all of these
-// variables is static.
-// MUTABLE GLOBAL VARS
-var glCanvas;               // HTML5 canvas DOM element, id="canvas"
-var gl;                     // WebGL context
-var squareVertexBuffer;     // Vertex buffer for drawing squares
-var vertexPositionAttribute // For sending vertices to shaders
-var shaderProgram;          // Shader program being used
+var VSHADER_SRC = `
+    precision highp float;
+    attribute vec2      a_position;
+    attribute vec2      a_texCoord;
+    uniform vec2        u_resolution;
+    varying vec2        v_texCoord;
 
-// Mozilla tutorials (and a lot of tutorials in general)
-//  embed the shaders as <script> elements in the html,
-//  then load them using a separate function, but since
-//  ECMAScript 6 allows for creating strings like this,
-//  I think it's easier and simple to just write the
-//  shader as part of the javascript file.
-//  NOTE: All caps vars are considered global constants.
-var VERTEX_SHADER_SOURCE = `
-    attribute vec3 vpos;
-
-    void main(void) {
-        gl_Position = vec4(vpos, 1.0);
-    }
-`;
-var FRAGMENT_SHADER_SOURCE = `
-    void main(void) {
-        gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+    void main() {
+        vec2 clipSpacePos = a_position / u_resolution * 2.0 - 1.0;
+        gl_Position = vec4(clipSpacePos * vec2(1.0, -1.0), 0.0, 1.0);
+        v_texCoord = a_texCoord;
     }
 `;
 
-// USES GLOBAL VARS
-//  glCanvas
-//  gl
-function initWebGLContext() {
-    try {
-        gl = glCanvas.getContext("experimental-webgl");
-        gl.viewportWidth = glCanvas.width;
-        gl.viewportHeight = glCanvas.height;
+var FSHADER_SRC = `
+    precision highp float;
+    uniform sampler2D   u_image;
+    varying vec2        v_texCoord;
 
-        // Optional clear() call, set canvas to black
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clearDepth(1.0);
-        gl.enable(gl.DEPTH_TEST);
-        gl.depthFunc(gl.LEQUAL);
-        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    } catch (e) {
-        console.error(e)
+    void main() {
+        //gl_FragColor = vec4(color, 1.0);
+        gl_FragColor = texture2D(u_image, v_texCoord);
     }
-    if (!gl) {
-        console.error("Failed to initialize WebGL.");
-        gl = null;
-        return;
-    }
-}
-
-// PARAMS
-//  vertexShaderSource: shader script as string
-//  fragmentShaderSource: shader script as string
-// USES GLOBAL VARS
-//  gl
-function initShaderProgram(vertexShaderSource, fragmentShaderSource) {
-    // Compile shaders, handle errors
-    // The compilation of individual shaders could be split into a
-    // separate function, but personally I think it's easier to
-    // handle errors like this. I'll leave it up to you guys if you
-    // would like to change it.
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSource);
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error(
-            "Error compiling vertex shader: " +
-            gl.getShaderInfoLog(vertexShader)
-        );
-        gl = null;
-        return;
-    }
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSource);
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error(
-            "Error compiling fragment shader: " +
-            gl.getShaderInfoLog(fragmentShader)
-        );
-        gl = null;
-        return;
-    }
-
-    // Link shaders into final program
-    shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.error("Unable to intialize the shader program.");
-        gl = null;
-        return;
-    }
-    gl.useProgram(shaderProgram);
-
-    // Initialize shader globals
-    vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vpos");
-    gl.enableVertexAttribArray(vertexPositionAttribute);
-}
-
-// USES
-//  gl
-//  squareVertexBuffer
-function initBuffers() {
-    try {
-        squareVertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexBuffer);
-
-        var vertices = [
-             1.0,  1.0, 0.0,
-            -1.0,  1.0, 0.0,
-             1.0, -1.0, 0.0,
-            -1.0, -1.0, 0.0,
-        ];
-
-        gl.bufferData(
-            gl.ARRAY_BUFFER,
-            new Float32Array(vertices),
-            gl.STATIC_DRAW
-        );
-    } catch (e) {
-        console.error(e);
-        gl = null;
-        return;
-    }
-}
-
-function draw() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexBuffer);
-    gl.vertexAttribPointer(
-        vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0
-    );
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-}
+`;
 
 function start() {
-    glCanvas = document.getElementById("canvas");
+    var image = new Image();
+    image.src = "images/checker_images.png";
+    image.onload = function() {
+        main(image);
+    }
+}
 
-    initWebGLContext(canvas);
+function main(image) {
+    var canvas = document.getElementById("canvas");
+    var gl = canvas.getContext("experimental-webgl");
     if (!gl) {
-        console.error("Error getting WebGL Context, exiting...");
         return;
     }
+    gl.viewport(0, 0, canvas.width, canvas.height);
 
-    initShaderProgram(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
-    if (!gl) {
-        console.error("Error compiling shaders, exiting...");
-        return;
+    var vshader = loadShader(gl, VSHADER_SRC, gl.VERTEX_SHADER);
+    var fshader = loadShader(gl, FSHADER_SRC, gl.FRAGMENT_SHADER);
+    var program = loadProgram(gl, [vshader, fshader]);
+    gl.useProgram(program);
+
+    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+    var texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+            0.0, 1.0,
+            1.0, 0.0,
+            1.0, 1.0,
+        ]),
+        gl.STATIC_DRAW
+    );
+    gl.enableVertexAttribArray(texCoordLocation);
+    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+    var texture = loadTexture(gl);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    loadQuad(gl, 0, 0, image.width, image.height);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+function loadQuad(gl, x, y, width, height) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            x, y,
+            x + width, y,
+            x, y + height,
+            x, y + height,
+            x + width, y,
+            x + width, y + height,
+        ]),
+        gl.STATIC_DRAW
+    );
+}
+
+function loadTexture(gl) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    return texture;
+}
+
+function loadShader(gl, shaderSource, shaderType) {
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, shaderSource);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        var error = gl.getShaderInfoLog(shader);
+        console.error("Error compiling shader: " + error);
+        gl.deleteShader(shader);
+        return null;
     }
-
-    initBuffers();
-    if (!gl) {
-        console.error("Error intializing vertex buffers, exiting...");
-        return;
+    return shader;
+}
+function loadProgram(gl, shaders, opt_attribs, opt_locations) {
+    var program = gl.createProgram();
+    for (var i = 0; i < shaders.length; ++i) {
+        gl.attachShader(program, shaders[i]);
     }
-
-    // Set draw loop
-    setInterval(draw, 15);
+    if (opt_attribs) {
+        for (var i = 0; i < opt_attribs.length; ++i) {
+            gl.bindAttribLocation(
+                program,
+                opt_locations ? opt_locations[i] : i,
+                opt_attribs[i]
+            );
+        }
+    }
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        var error = gl.getProgramInfoLog(program);
+        console.error("Error in program linking:" + error);
+        gl.deleteProgram(program);
+        return null;
+    }
+    return program;
 }
