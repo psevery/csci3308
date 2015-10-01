141bb571d6934a057ac1e44af060b61b830dd351
Patrick Severy <psevery33@yahoo.com>
2015-09-30 21:40:17 -0600

    You can move pieces now :)

--- DoubleJump/public/checkers.js
+++ DoubleJump/public/checkers.js
@@ -18,7 +18,12 @@ var INIT_BOARD =
   "20202020" +
   "02020202";
 
-
+var board = {
+  pieces: INIT_BOARD,
+  topx: 0,
+  topy: 20,
+  width: 9
+};
 // <body onload="start()">
 function start() {
 
@@ -31,19 +36,7 @@ function start() {
   board_img.src = "images/checkerboard.jpg";
   pieces_img = new Image();
   pieces_img.src = "images/checker_images.png";
-
-  var board = {
-    pieces: INIT_BOARD,
-    topx: 0,
-    topy: 20,
-    width: 9
-  };
-
   // Draw sprites
-  drawBoard(board);
-
-  // Testing addMove
-  addMove(board,"6251");
   drawBoard(board);
 }
 
@@ -88,6 +81,7 @@ function drawBoard(board) {
 function addMove(board,move) {
   //getting the indexes of the location in the pieces string of the start and 
   //destination squares
+  console.log("in addmove");
   from_index = (parseInt(move.charAt(0), 10)-1)*8+parseInt(move.charAt(1))-1;
   to_index = (parseInt(move.charAt(2), 10)-1)*8+parseInt(move.charAt(3))-1;
   //saves the piece type
@@ -96,6 +90,8 @@ function addMove(board,move) {
   board.pieces = board.pieces.substr(0,from_index) + "0" + board.pieces.substr(from_index+1);
   //fills the desination spot on the board
   board.pieces = board.pieces.substr(0,to_index) + piece_type + board.pieces.substr(to_index+1);
+  drawBoard(board);
+
 }
 
 
@@ -103,16 +99,78 @@ function addMove(board,move) {
 start();
 //has to be after start
 canvas.addEventListener("mousedown", getPosition, false);
-
+var themove = "";
 function getPosition(event)
 {
+  //want to find the row collumn pair
+  var row;
+  var collumn;
+  //geting the x and y location of the mouse
   var x = event.x;
   var y = event.y;
- // var canvas = document.getElementById("canvas");
+  //making x and y relative to the canvas 
   x -= (canvas.offsetLeft);
   y -= (canvas.offsetTop);
-  alert (x+ "," +y);
-  //using this get the square a user clicked
+  
+  //ugly way to get row and collumn, sorry
+    if (x < 80){
+      collumn = 1;
+    }
+    else if (x < 152){
+      collumn = 2;
+    }
+    else if (x < 224){
+      collumn = 3;
+    }
+    else if (x < 296){
+      collumn = 4;
+    }
+    else if (x < 370){
+      collumn = 5;
+    }
+    else if (x < 442){
+      collumn = 6;
+    }
+    else if (x < 516){
+      collumn = 7;
+    }
+    else if (x < 610){
+      collumn = 8;
+    }
+  
+    if (y < 103){
+      row = 1;
+    }
+    else if (y < 174){
+      row = 2;
+    }
+    else if (y < 249){
+      row = 3;
+    }
+    else if (y < 320){
+      row = 4;
+    }
+    else if (y< 391){
+      row = 5;
+    }
+    else if (y < 467){
+      row = 6;
+    }
+    else if (y < 538){
+      row = 7;
+    }
+    else if (y < 610){
+      row = 8;
+    }
+    themove = themove+row+collumn;
+    if (themove.length == 4){
+      console.log(themove);
+      addMove(board,themove);
+      themove = "";
+      console.log(themove);
+    }
+    
+  
 }
 
 
