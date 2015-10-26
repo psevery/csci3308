72a552ee0e4f0c84aa8533bdfa3d475a8a751d19
Patrick Severy <psevery33@yahoo.com>
2015-10-26 15:27:05 -0600

    ImBoard image fits correctly in canvas now.

--- DoubleJump/public/scripts/draw.js
+++ DoubleJump/public/scripts/draw.js
@@ -9,7 +9,8 @@ function loadImages() {
 function drawBoard(board, canvas, context) {
   context.clearRect(0,0,canvas.width,canvas.height);
   // The board is scalable based on the width and height of the canvas
-  context.drawImage(board_img, 0, 0, 600, 597, canvas.width/600*20, canvas.height/597*20, canvas.width, canvas.height);
+  //context.drawImage(board_img, 0, 0, 588, 590, canvas.width/588*20, canvas.height/590*20, canvas.width, canvas.height);
+  context.drawImage(board_img, 0, 0, canvas.width, canvas.height);
   // Loops through the board matrix and for each nonzer entry places a piece on the board
   for(i=0; i<8; i++) {
     for(j=0; j<8; j++) {
@@ -20,9 +21,9 @@ function drawBoard(board, canvas, context) {
         // should be drawn (8 squares on a row/column). An additional space is added to shift the starting location off of
         // one of the board's grey spaces.
         context.drawImage(pieces_img, 64 * (board.matrix[i][j] - 1), 0, 64, 64, 
-          ((canvas.width-(9/600)*canvas.width)/8)*j+(9/600)*canvas.width, 
-          ((canvas.height-(9/597)*canvas.height)/8)*i+(9/597)*canvas.height, 
-          64*canvas.width/600, 64*canvas.height/597);
+          ((canvas.width-(9/588)*canvas.width)/8)*j+(9/588)*canvas.width, 
+          ((canvas.height-(9/590)*canvas.height)/8)*i+(9/590)*canvas.height, 
+          64*canvas.width/588, 64*canvas.height/590);
       }
     }
   }
