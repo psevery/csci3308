27771e456b13a483092a55afe08a290831142c61
andrwhuang <andrew.huang@colorado.edu>
2015-12-05 12:27:33 -0700

    Adding connection to mongodb

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -1,6 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+
 var username;
 
 function initDatabase(){
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+  });
+
   username = window.prompt("Nickname","");
   var name = document.getElementById("user2");
   name.innerHTML = username;
